706eeba5cd0fac0a0ecc3ca8daaabe06ced6dcc4
Ricky O'Steen <rosteen@stsci.edu>
2020-11-30 10:15:31 -0500

    Deleted old custom_model code now that I'm using a Model subclass

--- HST/dispersion_models.py
+++ HST/dispersion_models.py
@@ -3,30 +3,11 @@ from astropy.modeling.models import custom_model
 from astropy.modeling import Model, Parameter
 from asdf.extension import Converter
 
-def create_dispxy_model(e, inverse=False):
-    """Return a custom astropy model defining the dispersion.
-
-    Equations here are specifically for WFC3 IR, taken from GRISMCONF's poly.py
-    file (specifically POLY12 and INVPOLY12). Note that the equations are of
-    the same form for both x and y, differing only in the input coefficient
-    matrix e.
-    """
-    e = np.array(e)
-    print(e)
-
-    @custom_model
-    def disp_model(x, y, t):
-        coeffs = [1, x, y, x**2, x*y, y**2]
-        if inverse:
-            return (t - np.dot(coeffs,e[0,:]))/np.dot(coeffs,e[1,:])
-        else:
-            return np.dot(coeffs,e[0,:]) + t*np.dot(coeffs,e[1,:])
-
-    return disp_model
-
 class DISPXY_Model(Model):
     n_inputs = 3
     n_outputs = 1
+    _tag = "tag:stsci.edu:grismstuff/dispxy_model-1.0.0"
+    _name = "DISPXY_Model"
 
     def __init__(self, ematrix, inv=False):
         self.ematrix = np.array(ematrix)
@@ -42,7 +23,7 @@ class DISPXY_Model(Model):
 
 class DISPXY_ModelConverter(Converter):
     tags = ["tag:stsci.edu:grismstuff/dispxy_model-*"]
-    types = ["mypackage.DispxyFunction"]
+    types = ["dispersion_models.DISPXY_Model"]
 
     def to_yaml_tree(self, obj, tags, ctx):
         # ASDF will know how to turn the nested lists into yaml properly
