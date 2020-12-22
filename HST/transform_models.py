183b95cedefbebdf3cead9ff60ddef9ebdb4a457
Ricky O'Steen <rosteen@stsci.edu>
2020-12-22 12:02:09 -0500

    Starting from NIRISS code instead of NIRCam

--- HST/transform_models.py
+++ HST/transform_models.py
@@ -32,9 +32,10 @@ class WFC3IRForwardGrismDispersion(Model):
 
     Notes
     -----
-    Based on the JWST transform model code, see
+    Based on the JWST NIRISS transform model code, see
     https://github.com/spacetelescope/jwst/blob/master/jwst/transforms/models.py
     """
+
     standard_broadcasting = False
     _separable = False
     fittable = False
@@ -44,69 +45,95 @@ class WFC3IRForwardGrismDispersion(Model):
     n_outputs = 4
 
     def __init__(self, orders, lmodels=None, xmodels=None,
-                 ymodels=None, name=None, meta=None):
-        self.orders = orders
-        self.lmodels = lmodels
+                 ymodels=None, theta=0., name=None, meta=None):
+        self._order_mapping = {int(k): v for v, k in enumerate(orders)}
         self.xmodels = xmodels
         self.ymodels = ymodels
-        self._order_mapping = {int(k): v for v, k in enumerate(orders)}
-        meta = {"orders": orders}  # informational for users
+        self.lmodels = lmodels
+        self.theta = theta
+        self.orders = orders
+        meta = {"orders": orders}
         if name is None:
-            name = 'wfc3ir_forward_grism_dispersion'
+            name = 'wfc3ir_forward_row_grism_dispersion'
         super(WFC3IRForwardGrismDispersion, self).__init__(name=name,
                                                               meta=meta)
+        # starts with the backwards pixel and calculates the forward pixel
         self.inputs = ("x", "y", "x0", "y0", "order")
         self.outputs = ("x", "y", "wavelength", "order")
 
     def evaluate(self, x, y, x0, y0, order):
-        """Return the transform from grism to image for the given spectral order.
-
+        """Return the valid pixel(s) and wavelengths given center x,y and lam
         Parameters
         ----------
-        x : float
-            input x pixel
-        y : float
-            intput y pixel
-        x0 : float
-            input x-center of object
-        y0 : float
-            input y-center of object
+        x0: int,float,list
+            Source object x-center
+        y0: int,float,list
+            Source object y-center
+        x :  int,float,list
+            Input x location
+        y :  int,float,list
+            Input y location
         order : int
-            the spectral order to use
+            Spectral order to use
+        Returns
+        -------
+        x, y, lambda, order, theta,  in the direct image for the pixel that was
+        specified as input using the wavelength l and spectral order
+        Notes
+        -----
+        There's spatial dependence for NIRISS as well as dependence on the
+        filter wheel rotation during the exposure.
         """
         try:
             iorder = self._order_mapping[int(order.flatten()[0])]
+        except AttributeError:
+            iorder = self._order_mapping[order]
         except KeyError:
             raise ValueError("Specified order is not available")
 
+        # The next two lines are to get around the fact that
+        # modeling.standard_broadcasting=False does not work.
+        #x00 = x0.flatten()[0]
+        #y00 = y0.flatten()[0]
+
+        t = np.linspace(0, 1, 10)  #sample t
         xmodel = self.xmodels[iorder]
         ymodel = self.ymodels[iorder]
         lmodel = self.lmodels[iorder]
 
-        # inputs are x, y, x0, y0, order
+        dx = xmodel.evaluate(x0, y0, t)
+        dy = ymodel.evaluate(x0, y0, t)
 
-        tmodel = astmath.SubtractUfunc() | xmodel
-        model = Mapping((0, 2, 0, 2, 2, 3, 4)) | ( tmodel | ymodel) & (tmodel | lmodel) & Identity(3) |\
-              Mapping((2, 3, 0, 1, 4)) | Identity(1) & astmath.AddUfunc() &  Identity(2) | Mapping((0, 1, 2, 3), n_inputs=4)
 
+        if self.theta != 0.0:
+            rotate = Rotation2D(self.theta)
+            dx, dy = rotate(dx, dy)
+
+        so = np.argsort(dx)
+        tab = Tabular1D(dx[so], t[so], bounds_error=False, fill_value=None)
+
+        dxr = astmath.SubtractUfunc()
+        wavelength = dxr | tab | lmodel
+        model = Mapping((2, 3, 0, 2, 4)) | Const1D(x00) & Const1D(y00) & wavelength & Const1D(order)
         return model(x, y, x0, y0, order)
+
+
 
 class WFC3IRBackwardGrismDispersion(Model):
     """Return the valid pixel(s) and wavelengths given center x,y and lam
 
     Parameters
     ----------
-    orders : list [int]
-        List of orders which are available
-
-    lmodels : list [astropy.modeling.Model]
-        List of models which govern the wavelength solutions
-
-    xmodels : list [astropy.modeling.Model]
-        List of models which govern the x solutions
-
-    ymodels : list [astropy.modeling.Model]
-        List of models which givern the y solutions
+    xmodels : list[tuple]
+        The list of tuple(models) for the polynomial model in x
+    ymodels : list[tuple]
+        The list of tuple(models) for the polynomial model in y
+    lmodels : list
+        The list of models for the polynomial model in l
+    orders : list
+        The list of orders which are available to the model
+    theta : float
+        Angle [deg] - defines the NIRISS filter wheel position
 
     Returns
     -------
@@ -127,54 +154,65 @@ class WFC3IRBackwardGrismDispersion(Model):
     n_outputs = 5
 
     def __init__(self, orders, lmodels=None, xmodels=None,
-                 ymodels=None, name=None, meta=None):
+                 ymodels=None, theta=None, name=None, meta=None):
         self._order_mapping = {int(k): v for v, k in enumerate(orders)}
-        self.lmodels = lmodels
         self.xmodels = xmodels
         self.ymodels = ymodels
+        self.lmodels = lmodels
         self.orders = orders
+        self.theta = theta
         meta = {"orders": orders}
         if name is None:
-            name = "wfc3ir_backward_grism_dispersion"
+            name = 'wfc3ir_backward_grism_dispersion'
         super(WFC3IRBackwardGrismDispersion, self).__init__(name=name,
                                                             meta=meta)
         self.inputs = ("x", "y", "wavelength", "order")
         self.outputs = ("x", "y", "x0", "y0", "order")
 
     def evaluate(self, x, y, wavelength, order):
-        """Return the transform from image to grism for the given spectral order.
-
+        """Return the valid pixel(s) and wavelengths given center x,y and lam
         Parameters
         ----------
-        x : float
-            input x pixel on the direct image
-        y : float
-            intput y pixel on the direct image
+        wavelength : int,float
+            Input wavelength you want to know about, will be converted to float
+        x :  int,float
+            Input x location
+        y :  int,float
+            Input y location
         wavelength : float
-            input wavelength in angstroms
-        order : int
-            specifies the spectral order
+            Wavelength to disperse
+        order : list
+            The order to use
+        Returns
+        -------
+        x, y, wavelength, order in the grism image for the pixel at x,y that was
+        specified as input using the wavelength and order specified
+        Notes
+        -----
+        There's spatial dependence for NIRISS so the forward transform
+        dependes on x,y as well as the filter wheel rotation. Theta is
+        usu. taken to be the different between fwcpos_ref in the specwcs
+        reference file and fwcpos from the input image.
         """
+        if (wavelength < 0).any():
+            raise ValueError("Wavelength should be greater than zero")
         try:
             iorder = self._order_mapping[int(order.flatten()[0])]
+        except AttributeError:
+            iorder = self._order_mapping[order]
         except KeyError:
             raise ValueError("Specified order is not available")
 
-        if (wavelength < 0).any():
-            raise ValueError("wavelength should be greater than zero")
-
-        # These should be dispx and dispy from the reference file
+        t = self.lmodels[iorder](wavelength)
         xmodel = self.xmodels[iorder]
         ymodel = self.ymodels[iorder]
-        # lmodel should be invdispl from the reference file
-        lmodel = self.lmodels[iorder]
 
-        # Convert lambda to t, then use that to get dx and dy to add to the
-        # input coordinates to get the dispersed coordinates
-        model = (Identity(2) * lmodel * Identity(1)) | \
-                Mapping((0, 1, 2, 0, 1, 2, 0, 1, 3)) | \
-                xmodel & ymodel & Identity(3) | \
-                Mapping((0, 2, 1, 3, 2, 3, 4)) | \
-                astmath.AddUfunc() & astmath.AddUfunc() & Identity(3)
+        dx = xmodel(x, y, t)
+        dy = ymodel(x, y, t)
 
-        return model(x, y, wavelength, order)
+        ## rotate by theta
+        if self.theta != 0.0:
+            rotate = Rotation2D(self.theta)
+            dx, dy = rotate(dx, dy)
+
+        return (x+dx, y+dy, x, y, order)
