4f9f4bf234e5ea4bba57adc878e0db971d3d6622
Ricky O'Steen <rosteen@stsci.edu>
2021-07-12 13:01:41 -0400

    Working on inverse Y dispersion handling for UVIS

--- HST/dispersion_models.py
+++ HST/dispersion_models.py
@@ -1,7 +1,29 @@
 import numpy as np
+from scipy.interpolate import interp1d
 from astropy.modeling.models import custom_model
 from astropy.modeling import Model, Parameter
 from asdf.extension import Converter
+
+
+class interp1d_picklable(object):
+    """ class wrapper for piecewise linear function
+    """
+
+    def __init__(self, xi, yi, **kwargs):
+        self.xi = xi
+        self.yi = yi
+        self.args = kwargs
+        self.f = interp1d(xi, yi, **kwargs)
+
+    def __call__(self, xnew):
+        return self.f(xnew)
+
+    def __getstate__(self):
+        return self.xi, self.yi, self.args
+
+    def __setstate__(self, state):
+        self.f = interp1d(state[0], state[1], **state[2])
+
 
 class DISPXY_Model(Model):
     n_inputs = 3
@@ -9,9 +31,10 @@ class DISPXY_Model(Model):
     _tag = "tag:stsci.edu:grismstuff/dispxy_model-1.0.0"
     _name = "DISPXY_Model"
 
-    def __init__(self, ematrix, offset, inv=False):
+    def __init__(self, ematrix, offset, inv=False, interpolate=False):
         self.ematrix = np.array(ematrix)
         self.inv = inv
+        self.interpolate = interpolate
         self.offset = offset
 
         if len(self.ematrix.shape) > 1:
@@ -21,7 +44,14 @@ class DISPXY_Model(Model):
                                           " for higher order than linear in t")
 
     # Note that in the inverse case, input "t" here is actually dx or dy
-    def evaluate(self, x, y, t):
+    # t0 is only used in the case of interpolating for an inverse transform
+    def evaluate(self, x, y, t, t0=np.linspace(-1,2,40), inv=None,interpolate=None):
+        # Need this to allow override of inverse and interpolate for recursion
+        if inv is None:
+            inv = self.inv
+        if interpolate is None:
+            interpolate = self.interpolate
+
         e = self.ematrix
         offset = self.offset
         coeffs = {1: np.array([1]),
@@ -32,9 +62,15 @@ class DISPXY_Model(Model):
 
         f = 0
 
-        if self.inv:
-            f = ((t + offset - np.dot(coeffs[c_order], e[0,:])) /
-                 np.dot(coeffs[c_order], e[1,:]))
+        if inv:
+            if interpolate:
+                xr, yr = self.evaluate(x, y, t0, inv=False)
+                so = np.argsort(yr)
+                interpolation = interp1d_picklable(yr[so], t0[so])
+                f = interpolation(t)
+            else:
+                f = ((t + offset - np.dot(coeffs[c_order], e[0,:])) /
+                     np.dot(coeffs[c_order], e[1,:]))
         else:
             for i in range(0, t_order):
                 f += t**i * (np.dot(coeffs[c_order], e[i,:]))
